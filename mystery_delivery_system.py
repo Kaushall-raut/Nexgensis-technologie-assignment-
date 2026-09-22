576cc3d683e4520ca507011ca2f2518372b2e4b0
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-09-22 21:09:39 +0530

    add function to calculate euclidean distance

--- mystery_delivery_system.py
+++ mystery_delivery_system.py
@@ -1,9 +1,16 @@
 import json   # module for reading and converting json 
-
+import math
 
 with open('base_case.json',"r") as json_file:
     data=json.load(json_file)     # loading json file and storing it into a variable 
 
 
-print(data['warehouses'][0])
 
+
+# function for calculating euclidean distance 
+
+def euclidean_distance(point1,point2):
+    distance=math.sqrt(((point1[0]-point2[0])**2) + ((point1[1]-point2[1])**2) )
+    return distance
+
+print(euclidean_distance([5,5],[0,0]))
